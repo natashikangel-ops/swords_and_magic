500fb505c430bc637ac3cec02966a88627dcc430
Наталья Овчинникова <natashik.angel@gmail.com>
2026-07-08 22:18:29 +0500

    hurra

--- bin/lesson4_19.dart
+++ bin/lesson4_19.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 void main() {
-  
   String infix = stdin.readLineSync() ?? '';
 
   if (infix.isEmpty || infix == '0') {
@@ -9,95 +8,65 @@ void main() {
     return;
   }
 
-/*
-  final prioritet = {'+': 0, '-': 0, '*': 1, '/': 1};
+  print(infixToPostfix(infix));
+}
 
-  List<String> operators = ['+', '-', '*', '/'];
-
+String infixToPostfix(String expression) {
+  
+  List<String> output = [];
   List<String> stack = [];
-  List<dynamic> queue = [];
 
-  for (var i = 0; i < infix.length; i++) {
-    String simbol = infix[i];
+  for (int i = 0; i < expression.length; i++) {
+    String symbol = expression[i];
 
-    int num = int.tryParse(simbol) ?? 0;
-
-    if (num == 0) {
-      if (operators.contains(simbol)) {
-
-        if (stack.isEmpty || simbol == '(') {
-          stack.add(simbol);
-        } else {
-          String top = stack[stack.length - 1];
-          
-          if (top == '(') {
-            stack.add(simbol);
-            continue;
-          }
-
-          if (prioritet[simbol]! > prioritet[top]!) {
-            stack.add(simbol);
-          } else {
-            
-            for (int i = stack.length - 1; i >= 0; i--) {
-              if (stack[i] == '(' ||
-                  prioritet[simbol]! > prioritet[stack[i]]!) {
-                    
-                break;
-              } else {
-                queue.add(stack[i]);
-                stack.removeAt(i);                
-              }
-            }
-            stack.add(simbol);
-          }
-        }
-      } else if (simbol == '(') {
-        stack.add(simbol);
-      } else if (simbol == ')') {
-        for (int i = stack.length - 1; i >= 0; i--) {
-          if (stack[i] == '(') {
-            stack.removeAt(i);
-            break;
-          } else {
-            queue.add(stack[i]);
-            stack.removeAt(i);
-          }
-        }
-      } else {
-        print('Неправильное значение');
-        return;
+    if (isDigit(symbol)) {
+      output.add(symbol);
+    } else if (isOperator(symbol)) {
+      while (stack.isNotEmpty &&
+          isOperator(stack.last) &&
+          priority(stack.last) >= priority(symbol)) {
+        output.add(stack.removeLast());
       }
-    } else {
-      queue.add(num);
+      stack.add(symbol);
+    } else if (symbol == '(') {
+      stack.add(symbol);
+    } else if (symbol == ')') {
+      while (stack.isNotEmpty && stack.last != '(') {
+        output.add(stack.removeLast());
+      }
+
+      if (stack.isNotEmpty && stack.last == '(') {
+        stack.removeLast();
+      }
     }
   }
 
-  queue.addAll(stack);
+  while (stack.isNotEmpty) {
+    output.add(stack.removeLast());
+  }
 
-  print(queue.join());
-*/
-
-  List<String> stack = [];
-  List<dynamic> queue = [];
-
-for (var i = 0; i < infix.length; i++) {
-    String simbol = infix[i];
-
-    int num = int.tryParse(simbol) ?? 0;
-
-    if (num == 0) {
-      switch (simbol) {
-      case '(':
-        stack.add(simbol);  
-      default:
-    } 
-    } else {
-      stack.add(simbol);
-    }
-
-    
+  return output.join();
 }
 
+int priority(String op) {
+  switch (op) {
+    case '+':
+    case '-':
+      return 1;
+    case '*':
+    case '/':
+      return 2;
+    default:
+      return 0;
+  }
+}
 
+// цифра ли это
+bool isDigit(String symbol) {
+  return int.tryParse(symbol) != null;
+}
+
+// оператор ли это
+bool isOperator(String symbol) {
+  return symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/';
 }
